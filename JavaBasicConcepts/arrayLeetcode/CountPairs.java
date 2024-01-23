195b0f23c752ff1be3d1ed290fa5e081c9a3b8ba
rdc07 <rdchilhorkar@gmail.com>
2024-01-23 20:02:37 +0530

    Modified the code

--- JavaBasicConcepts/arrayLeetcode/CountPairs.java
+++ JavaBasicConcepts/arrayLeetcode/CountPairs.java
@@ -1,32 +1,27 @@
 import java.util.*;
 
 public class CountPairs {
-    public static int countPairs(int arr[], int target) {
+    public static int countPairs(int[] arr, int target){
+
         Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
-
-        // Count the frequency of each element in the array
-        for (int i = 0; i < arr.length; i++) {
+        for(int i=0; i<arr.length; i++){
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
 
-        // Count pairs with the given sum
-        for (int i = 0; i < arr.length; i++) {
-            if (map.containsKey(target - arr[i])) {
-                count += map.get(target - arr[i]);
-
-                // Avoid counting the same pair twice
-                if (arr[i] * 2 == target) {
-                    count--;
-                }
+        for(int i=0; i<arr.length; i++){
+            if(map.containsKey(target - arr[i])){
+                count += map.get(arr[i]);
             }
-        }
 
-        return count / 2; // Each pair is counted twice, so divide by 2
+            if( arr[i] * 2 == target)
+                count--;
+        }
+        return count/2;
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 1, 5};
+        int arr[] = {3,3,3,2,5};
         int target = 6;
 
         int count = countPairs(arr, target);
